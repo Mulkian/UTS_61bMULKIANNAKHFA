c5aca7c2466d2529101aa41bfc11b572c7781a39
Mulkian <145630818+Mulkian@users.noreply.github.com>
2024-05-18 01:26:28 +0700

    add databases

---
+++ database/migrations/2024_05_17_181414_create_penyewas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penyewas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('tanggal');
+            $table->string('plat_nomer');
+            $table->string('jenis_mobil');
+            $table->string('harga');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penyewas');
+    }
+};
